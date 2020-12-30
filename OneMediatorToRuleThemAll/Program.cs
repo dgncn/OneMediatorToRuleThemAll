50dea790cf3e9014a9f5c3ef12ec16ccf4859210
Dogancan Kasap <dgncn.ksp@gmail.com>
2020-12-31 01:20:21 +0300

    Add project files.

---
+++ OneMediatorToRuleThemAll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OneMediatorToRuleThemAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
